3b4390d5e010990633e28c4d84efcd440f52e125
Pavel Kozlov <it.pavelkozlov@gmail.com>
2019-06-14 09:43:13 +0300

    Homework dynamic-templates. Bug fixed.

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -6,12 +6,13 @@ from django.conf import settings
 def inflation_view(request):
     template_name = 'inflation.html'
     with open(settings.CSV_FILE) as f:
-        fieldnames = ['year', 'jan', 'feb', 'march', 'apr', 'may', 'jun', 'july', 'aug', 'sept', 'oct', 'nov', 'dec', 'summ']
+        fieldnames = f.readline().strip().split(';')
         reader = csv.DictReader(f, fieldnames=fieldnames, delimiter=';')
         result = list(dict(i) for i in reader)
 
     context = {
-        'rows': result
+        'rows': result,
+        'fieldnames': fieldnames
     }
 
     return render(request, template_name, context=context)
